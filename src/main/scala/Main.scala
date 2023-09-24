674e4b2a7dde9e45f8e5f73bcb53e8d43bdce27c
Pisarevskiy.RE <daimon--@live.ru>
2023-09-24 22:40:01 +0300

    done

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,30 +1,13 @@
 import metrics.{AverageSleepDurationPerDay, MinMaxSleepDurationPerDay, SleepTimeWindow}
 import org.apache.flink.api.common.typeinfo.{TypeInformation, Types}
 import org.apache.flink.core.fs.Path
+import org.apache.flink.streaming.api.datastream.DataStreamSource
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
-import reader.{CsvReader, CsvSchemaBuilder}
-import scheme._
-import transforms.{ConverterToAppUsage, EventAggregator, EventMerge, ExtractDayOfWeek, FilterEvent, KeyDayOfWeekSelector, KeyEventSelector, RenameEvents, Watermark}
-import org.apache.flink.api.common.eventtime.{SerializableTimestampAssigner, WatermarkStrategy}
-import org.apache.flink.api.common.functions.{AggregateFunction, MapFunction}
-import org.apache.flink.api.common.state.{ValueState, ValueStateDescriptor}
-import org.apache.flink.api.common.typeinfo.Types
-import org.apache.flink.api.java.functions.KeySelector
-import org.apache.flink.configuration.Configuration
-import org.apache.flink.connector.file.src.FileSource
-import org.apache.flink.core.fs.Path
-import org.apache.flink.formats.csv.CsvReaderFormat
-import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.annotation.JsonProperty
-import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.dataformat.csv.CsvSchema
-import org.apache.flink.streaming.api.datastream.{DataStreamSource, SingleOutputStreamOperator}
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
-import org.apache.flink.streaming.api.functions.KeyedProcessFunction
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows
 import org.apache.flink.streaming.api.windowing.time.Time
-import org.apache.flink.util.Collector
-
-import java.time.format.DateTimeFormatter
-import java.time.{Duration, LocalTime, ZoneId, ZonedDateTime}
+import reader.{CsvReader, CsvSchemaBuilder}
+import scheme._
+import transforms._
 
 object Main extends App {
 
@@ -81,7 +64,6 @@ object Main extends App {
   averageSleepDurationPerDay.print("AverageSleepDurationPerDay -> ")
   sleepTimeWindow.print("SleepTimeWindow -> ")
   minMaxSleepDurationPerDay.print("minMaxSleepDurationPerDay -> ")
-  env.execute()
 
-
+  env.execute("SleepApp")
 }
--- src/main/scala/transforms/EventMerge.scala
+++ src/main/scala/transforms/EventMerge.scala
@@ -1,6 +1,7 @@
 package transforms
 
-import org.apache.flink.api.common.state.{ValueState, ValueStateDescriptor}
+import org.apache.flink.api.common.state.{StateTtlConfig, ValueState, ValueStateDescriptor}
+import org.apache.flink.api.common.time.Time
 import org.apache.flink.configuration.Configuration
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction
 import org.apache.flink.util.Collector
@@ -8,10 +9,16 @@ import scheme.AppUsage
 
 class EventMerge extends KeyedProcessFunction[Int, AppUsage, AppUsage]{
 
+  val ttlConfig = StateTtlConfig
+    .newBuilder(Time.minutes(1))
+    .setUpdateType(StateTtlConfig.UpdateType.OnCreateAndWrite)
+    .build
+
   private var previousAppUsage: ValueState[AppUsage] = _
 
   override def open(parameters: Configuration): Unit = {
     val previousAppNameDescriptor: ValueStateDescriptor[AppUsage] = new ValueStateDescriptor("previousAppUsage", classOf[AppUsage])
+    previousAppNameDescriptor.enableTimeToLive(ttlConfig)
     previousAppUsage = getRuntimeContext.getState(previousAppNameDescriptor)
   }
 
